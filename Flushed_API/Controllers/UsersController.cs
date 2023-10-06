d359003d9b97183b7c384d9a8f8bf51c7884ac88
zep1994 <thomasmatlockbba@gmail.com>
2023-10-06 11:28:23 -0500

    Add User Registration

--- Flushed_API/Controllers/UsersController.cs
+++ Flushed_API/Controllers/UsersController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Flushed_API.Dtos;
+using Flushed_API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System;
+using System.Text;
+
 
 namespace Flushed_API.Controllers
 {
-    public class UsersController : Controller
+    [ApiController]
+    public class AccountController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AccountController(UserManager<User> userManager, JwtSettings jwtSettings)
         {
-            return View();
+            _userManager = userManager;
+            _jwtSettings = jwtSettings;
+        }
+
+        [HttpPost]
+        [Route("api/register")]
+        public async Task<IActionResult> Register(UserRegistrationDto registration)
+        {
+            var user = new User
+            {
+                UserName = registration.Email,
+                Email = registration.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, registration.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new { errors = result.Errors });
+            }
+
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Key);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim("id", user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                // ... other claims ...
+            }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(descriptor);
+            return Ok(new { token = tokenHandler.WriteToken(token) });
         }
     }
 }
--- Flushed_API/Program.cs
+++ Flushed_API/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Flush_API.Data;
+using Flushed_API.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +14,50 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+var jwtSettings = new JwtSettings();
+builder.Configuration.Bind(nameof(JwtSettings), jwtSettings);
+builder.Services.AddSingleton(jwtSettings);
+
+var key = Encoding.ASCII.GetBytes(jwtSettings.Key);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = false,
+        ValidateLifetime = true
+    };
+});
+
+builder.Services.AddHttpClient();
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
